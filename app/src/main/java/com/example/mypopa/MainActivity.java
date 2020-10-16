3e0164fa535071f0be77d16791f64a2f9b9e0f8d
Dipen Ghale <dipenghale@gmail.com>
2020-10-16 15:02:09 +0800

    using new class and validation, using checkbox etc

--- app/src/main/java/com/example/mypopa/MainActivity.java
+++ app/src/main/java/com/example/mypopa/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.mypopa;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -20,13 +27,13 @@ public class MainActivity extends AppCompatActivity {
     Spinner country;
     RadioButton male,female,others;
     TextView error_msg;
+    CheckBox cbox_music, cbox_game,cbox_book;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         country = findViewById(R.id.country);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.country_value,android.R.layout.simple_spinner_item);
@@ -47,12 +54,16 @@ public class MainActivity extends AppCompatActivity {
         female = findViewById(R.id.rd_female);
         others = findViewById(R.id.rd_others);
         error_msg = findViewById(R.id.error_msg);
+        cbox_music = findViewById(R.id.cbox_music);
+        cbox_game = findViewById(R.id.cbox_game);
+        cbox_book = findViewById(R.id.cbox_book);
 
         submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 String fullname, eid, pd, pnumstring, gender, cn;
-                String email_pattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                String interest = "";
+
                 int pnum;
                 fullname = full_name.getText().toString();
                 eid = email.getText().toString();
@@ -71,9 +82,30 @@ public class MainActivity extends AppCompatActivity {
                  */
                 gender = (male.isChecked()) ? "Male" : (female.isChecked()) ? "Female" : (others.isChecked()) ? "Others" : "";
                 cn = country.getSelectedItem().toString();
+                /*
+                if(cbox_music.isChecked()){
+                    interest +=cbox_music.getText().toString();
+                }
+                if(cbox_game.isChecked()){
+                    interest +=", "+cbox_game.getText().toString();
+                }
+
+                if(cbox_book.isChecked()){
+                    interest +=", "+cbox_book.getText().toString();
+                }
+
+                error_msg.setText(interest);
+                return;
+                */
+
+
+               EmailValidation ev = new EmailValidation();
 
                 if (fullname.length() == 0) {
-                    error_msg.setText("Please, Enter Full Name.");
+                    //error_msg.setText("Please, Enter Full Name.");
+                    full_name.setError("Enter Full Name");
+                    full_name.requestFocus();
+
                     return;
                 }
                 else{
@@ -82,9 +114,9 @@ public class MainActivity extends AppCompatActivity {
                     error_msg.setText("Please, Enter Email ID.");
                     return;
                 } else {
-                    if (!eid.trim().matches(email_pattern)) {
-
-                        error_msg.setText("Invalid Email Address.");
+                    if (ev.isEmailValid(eid)==false) {
+                        email.setError("Invalid Email Address.");
+                        email.requestFocus();
                         return;
                     } else {
 
@@ -114,7 +146,9 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }
+
             }
+
 
 
                 //error_msg.setText("Country Selected: "+cn);
@@ -131,6 +165,25 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+    //hide keyboard on outside touch
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View view = getCurrentFocus();
+        if (view != null && (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) && view instanceof EditText && !view.getClass().getName().startsWith("android.webkit.")) {
+            int[] scrcoords = new int[2];
+            view.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + view.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + view.getTop() - scrcoords[1];
+            if (x < view.getLeft() || x > view.getRight() || y < view.getTop() || y > view.getBottom())
+                hideKeyboard(this);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public static void hideKeyboard(Activity act) {
+        if(act!=null)
+            ((InputMethodManager)act.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow((act.getWindow().getDecorView().getApplicationWindowToken()), 0);
     }
 
 
